979fd0ab2faadd4ee3facdf1e0fd7b909bf8d8dc
kkyeer <kkyeer@gmail.com>
2020-12-02 11:50:19 +0800

    ci: 使用外部传入的key

--- upload.js
+++ upload.js
@@ -1,14 +1,15 @@
 let client = require('scp2');
 let os = require('os')
 let path = require('path')
-
+const args = process.argv.slice(2)
+const privateKey = args[0]==undefined?require('fs').readFileSync(os.homedir()+path.sep+'.ssh'+path.sep+'id_rsa'):args[0]
 // 服务器配置  https://github.com/mscdex/ssh2
 // scp2 主页 https://www.npmjs.com/package/scp2
 client.scp("public",{
   port:22,
   host:'www.tpfuture.top',
   username: 'ngmng',
-  privateKey: require('fs').readFileSync(os.homedir()+path.sep+'.ssh'+path.sep+'id_rsa'),
+  privateKey: privateKey,
   path:'/home/ngmng/data/dist/'
 },function (error) {
   console.log(error)
