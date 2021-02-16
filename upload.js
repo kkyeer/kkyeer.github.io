f081f3e9386f4fa9e63c347dc9fbb3f2363eeeb4
kkyeer <kkyeer@gmail.com>
2021-02-16 22:52:22 +0800

    ci: 压缩上传

--- upload.js
+++ upload.js
@@ -5,7 +5,7 @@ const args = process.argv.slice(2)
 const privateKey = args[0]==undefined?require('fs').readFileSync(os.homedir()+path.sep+'.ssh'+path.sep+'id_rsa'):args[0]
 // 服务器配置  https://github.com/mscdex/ssh2
 // scp2 主页 https://www.npmjs.com/package/scp2
-client.scp("/tmp/blog.tar",{
+client.scp("/tmp/blog.tar.xz",{
   port:22,
   host:'www.tpfuture.top',
   username: 'ngmng',
