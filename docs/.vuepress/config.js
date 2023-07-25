0a3294485e0dae1f06b2427b040e2facba04f045
kkyeer <kkyeer@gmail.com>
2023-07-25 02:52:21 +0000

    feat: 修改CDN

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -161,7 +161,7 @@ module.exports = {
     if(NODE_ENV === 'production'){
       return {
         output: {
-          publicPath: 'https://cdn.jsdelivr.net/gh/kkyeer/blog@gh-pages/'
+          publicPath: 'https://cdn.jsdelivr.net/gh/kkyeer/kkyeer.github.io@gh-pages/'
         },
         resolve: {
           //配置路径别名
